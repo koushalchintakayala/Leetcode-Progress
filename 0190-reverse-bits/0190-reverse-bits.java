8a439dd6866852757d5360294369ad7c7de65f9d
Koushal Ch <kc2828@srmist.edu.in>
2025-07-28 23:37:39 +0530

    Time: 0 ms (100%), Space: 40.8 MB (93.39%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,12 +1,12 @@
 class Solution {
     public int reverseBits(int n) {
-        int reverse = 0;
+        int result = 0;
 
         for (int i = 0; i < 32; i++) {
-            reverse <<= 1;
-            reverse |= (n & 1);
+            result <<= 1;
+            result |= (n & 1);
             n >>= 1;
         }
-        return reverse;
+        return result;
     }
 }
