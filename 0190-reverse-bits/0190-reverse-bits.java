71c57a27f83863f1ae33d67ff52a4b0b8956c8a3
Koushal Ch <kc2828@srmist.edu.in>
2025-07-28 23:36:48 +0530

    Time: 0 ms (100%), Space: 41 MB (87.74%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int reverseBits(int n) {
+        int reverse = 0;
+
+        for (int i = 0; i < 32; i++) {
+            reverse <<= 1;
+            reverse |= (n & 1);
+            n >>= 1;
+        }
+        return reverse;
+    }
+}
