68fb3ac04c8ccf0809a9b312b5387381b061760a
Koushal Chintakayala <193587157+koushalchintakayala@users.noreply.github.com>
2026-09-04 10:01:09 +0530

    Time: 2 ms (99.35%), Space: 47.4 MB (11.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,6 +5,7 @@ class Solution {
 
         for (int i = 0; i < n; i++) {
             int complement = target - nums[i];
+            
             if (numMap.containsKey(complement)) {
                 return new int[]{numMap.get(complement), i};
             }
